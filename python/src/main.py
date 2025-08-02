e689021ac827cafe86e34cad3c88a696e3b3650b
Anupam <anupaminit@gmail.com>
2025-08-03 02:38:43 +0530

    refactor: add comments

--- python/src/main.py
+++ python/src/main.py
@@ -9,10 +9,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    """ Brief summary of what the function does. """
     return render_template("index.html")
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    """ Brief summary of what the function does. """
     if request.method == "POST":
         if valid_login(request.form["t1"], request.form["t2"]):
             session["username"] = request.form["t1"]
@@ -24,13 +26,16 @@ def login():
 
 @app.route("/home")
 def home():
+    """ Brief summary of what the function does. """
     return render_template("home.html", user = session["username"])
 
 @app.route("/logout")
 def logout():
+    """ Brief summary of what the function does. """
     session.pop("username", None)
     return redirect(url_for("login"))
 
 @app.route("/maintenance")
 def maintenance():
+    """ Brief summary of what the function does. """
     return "Sorry, we are under maintenance. Try to login with same username and password."
