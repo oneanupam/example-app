e14f7b1d827fa4207897f6087de0655cc134cb8c
anupam. <43881706+oneanupam@users.noreply.github.com>
2025-08-03 02:39:24 +0530

    refactor: kzb-24 add comments (#28)
    * chore: add editorconfig
    
    * chore: kzb-24 update readme
    
    * feat: disabled gh actions
    
    * feat: add dockerfile for tst and tmp
    
    * refactor: add comments

--- python/run.py
+++ python/run.py
@@ -1,4 +1,16 @@
+"""
+This module is used to call the flask code.
+"""
+
 from src.main import app
+
+# Flask has the built-in Werkzeug server.
+# Only run the dev server if this file is executed directly
+
+# host='0.0.0.0' -> Exposes the app to all network interfaces, not just localhost.
+# port=8080	-> Binds the app to port 8080.
+# debug=True -> Enables debug mode, which provides automatic reload + debugger.
+# threaded=True	-> Enables multi-threading, so one process can handle multiple requests concurrently
 
 if __name__ == "__main__":
     app.debug = True
